c191ec9a204ab97739774222661b063425434f63
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-08-03 22:46:56 +0530

    Time: 0 ms (100%), Space: 8 MB (27.24%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        int xori=start^goal;
+        int count=0;
+        while(xori!=0)
+        {
+            xori=xori&(xori-1);
+            count++;
+        }
+        return count;
+        
+    }
+};
