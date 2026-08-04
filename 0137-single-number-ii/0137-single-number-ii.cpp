c7a1c457f89e2293ab688f6ab168efb1f1ca5604
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-08-04 22:26:53 +0530

    Time: 0 ms (100%), Space: 14.1 MB (11.02%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+       unordered_map <int,int> mp;
+       int ans;
+       for(int i=0;i<nums.size();i++)
+       {
+        mp[nums[i]]++;
+       }
+       for(auto it :mp)
+       {
+        if(it.second==1)
+        ans=it.first;
+       }
+       return ans;
+        
+    }
+};
