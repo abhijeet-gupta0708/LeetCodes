16b8d7b0bd243fbf9b0c31133052385d58d6fc5a
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-09-22 15:26:56 +0530

    Time: 705 ms (5%), Space: 11.4 MB (84.99%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,42 @@
+bool isfreqsame(int c1[],int c2[])
+{
+    for(int i=0;i<26;i++)
+    {
+        if(c1[i]!=c2[i])
+        return false;
+    }
+    return true;
+}
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+        // We will be using same technique as permutation string question
+
+        vector<int>ans;
+        int freq1[26]={0};
+
+        for(int i=0;i<p.size();i++)
+        freq1[p[i]-'a']++;
+
+        for(int j=0;j<s.size();j++)
+        {
+            int freq2[26]={0};
+            int ind=j;
+            int windsize=0;
+
+            while(windsize<p.size() && ind <s.length())
+            {
+                freq2[s[ind]-'a']++;
+                windsize++;
+                ind++;
+            }
+            // Checking if permutation exist
+
+            if(isfreqsame(freq1,freq2))
+            ans.push_back(j);
+
+        }
+        return ans;
+    }
+};
