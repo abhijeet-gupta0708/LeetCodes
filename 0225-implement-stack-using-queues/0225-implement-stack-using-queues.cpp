ffe19fe8b12128d34649a72a359cb4243d16f89f
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-09-24 07:47:45 +0530

    Time: 0 ms (100%), Space: 10.1 MB (21.88%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,32 +1,32 @@
 class MyStack {
 public:
+
     queue<int>q1;
     queue<int>q2;
     MyStack() {
         
     }
     
-    void push(int x) 
-    {
+    void push(int x) {
+
         while(!q1.empty())
         {
             q2.push(q1.front());
             q1.pop();
-
         }
+
         q1.push(x);
+
         while(!q2.empty())
         {
             q1.push(q2.front());
             q2.pop();
-
         }
-
-        
         
     }
     
     int pop() {
+
         int ele=q1.front();
         q1.pop();
         return ele;
@@ -42,7 +42,6 @@ public:
     bool empty() {
 
         return q1.empty();
-
         
     }
 };
